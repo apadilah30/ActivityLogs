8f83211b2e8d391d847823dfd437375977162b4f
apadilah30 <aguspadilah30@gmail.com>
2024-02-13 00:07:39 +0700

    Add project files.

---
+++ ActivityLogs/Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ListView1 = New ListView()
+        ColumnHeader1 = New ColumnHeader()
+        id = New ColumnHeader()
+        ColumnHeader2 = New ColumnHeader()
+        Harga = New ColumnHeader()
+        Stok = New ColumnHeader()
+        Satuan = New ColumnHeader()
+        btnAdd = New Button()
+        btnEdit = New Button()
+        btnDelete = New Button()
+        txtNama = New TextBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtStok = New NumericUpDown()
+        Label4 = New Label()
+        MySqlCommand1 = New MySql.Data.MySqlClient.MySqlCommand()
+        btnSave = New Button()
+        txtLogs = New RichTextBox()
+        Label5 = New Label()
+        Label6 = New Label()
+        txtSatuan = New TextBox()
+        txtHarga = New NumericUpDown()
+        CType(txtStok, ComponentModel.ISupportInitialize).BeginInit()
+        CType(txtHarga, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(258, 30)
+        Label1.TabIndex = 1
+        Label1.Text = "Warehouse Management"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Columns.AddRange(New ColumnHeader() {ColumnHeader1, id, ColumnHeader2, Harga, Stok, Satuan})
+        ListView1.FullRowSelect = True
+        ListView1.GridLines = True
+        ListView1.HoverSelection = True
+        ListView1.Location = New Point(12, 53)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(776, 218)
+        ListView1.TabIndex = 2
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' ColumnHeader1
+        ' 
+        ColumnHeader1.Text = "No"
+        ' 
+        ' id
+        ' 
+        id.Text = "Kode"
+        ' 
+        ' ColumnHeader2
+        ' 
+        ColumnHeader2.Text = "Nama"
+        ColumnHeader2.Width = 250
+        ' 
+        ' Harga
+        ' 
+        Harga.Text = "Harga"
+        Harga.Width = 250
+        ' 
+        ' Stok
+        ' 
+        Stok.Text = "Stok"
+        Stok.Width = 100
+        ' 
+        ' Satuan
+        ' 
+        Satuan.Text = "Satuan"
+        Satuan.Width = 100
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(12, 288)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 4
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnEdit
+        ' 
+        btnEdit.Location = New Point(110, 288)
+        btnEdit.Name = "btnEdit"
+        btnEdit.Size = New Size(75, 23)
+        btnEdit.TabIndex = 5
+        btnEdit.Text = "Edit"
+        btnEdit.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(211, 288)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 6
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' txtNama
+        ' 
+        txtNama.Location = New Point(12, 370)
+        txtNama.Name = "txtNama"
+        txtNama.Size = New Size(173, 23)
+        txtNama.TabIndex = 8
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 352)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(39, 15)
+        Label2.TabIndex = 9
+        Label2.Text = "Nama"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(191, 352)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(39, 15)
+        Label3.TabIndex = 11
+        Label3.Text = "Harga"
+        ' 
+        ' txtStok
+        ' 
+        txtStok.Location = New Point(405, 371)
+        txtStok.Maximum = New Decimal(New Integer() {-727379968, 232, 0, 0})
+        txtStok.Name = "txtStok"
+        txtStok.Size = New Size(71, 23)
+        txtStok.TabIndex = 13
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(405, 353)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(30, 15)
+        Label4.TabIndex = 14
+        Label4.Text = "Stok"
+        ' 
+        ' MySqlCommand1
+        ' 
+        MySqlCommand1.CacheAge = 0
+        MySqlCommand1.Connection = Nothing
+        MySqlCommand1.EnableCaching = False
+        MySqlCommand1.Transaction = Nothing
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Location = New Point(655, 371)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(133, 23)
+        btnSave.TabIndex = 15
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' txtLogs
+        ' 
+        txtLogs.Location = New Point(12, 435)
+        txtLogs.Name = "txtLogs"
+        txtLogs.ReadOnly = True
+        txtLogs.Size = New Size(776, 113)
+        txtLogs.TabIndex = 16
+        txtLogs.Text = ""
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(12, 417)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(32, 15)
+        Label5.TabIndex = 17
+        Label5.Text = "Logs"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(482, 352)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(43, 15)
+        Label6.TabIndex = 19
+        Label6.Text = "Satuan"
+        ' 
+        ' txtSatuan
+        ' 
+        txtSatuan.Location = New Point(482, 370)
+        txtSatuan.Name = "txtSatuan"
+        txtSatuan.Size = New Size(167, 23)
+        txtSatuan.TabIndex = 18
+        ' 
+        ' txtHarga
+        ' 
+        txtHarga.Location = New Point(191, 370)
+        txtHarga.Maximum = New Decimal(New Integer() {1000000000, 0, 0, 0})
+        txtHarga.Name = "txtHarga"
+        txtHarga.Size = New Size(208, 23)
+        txtHarga.TabIndex = 20
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 560)
+        Controls.Add(txtHarga)
+        Controls.Add(Label6)
+        Controls.Add(txtSatuan)
+        Controls.Add(Label5)
+        Controls.Add(txtLogs)
+        Controls.Add(btnSave)
+        Controls.Add(Label4)
+        Controls.Add(txtStok)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(txtNama)
+        Controls.Add(btnDelete)
+        Controls.Add(btnEdit)
+        Controls.Add(btnAdd)
+        Controls.Add(ListView1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(txtStok, ComponentModel.ISupportInitialize).EndInit()
+        CType(txtHarga, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListView1 As ListView
+    Public WithEvents btnAdd As Button
+    Public WithEvents btnEdit As Button
+    Public WithEvents btnDelete As Button
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents Harga As ColumnHeader
+    Friend WithEvents Stok As ColumnHeader
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtStok As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents MySqlCommand1 As MySql.Data.MySqlClient.MySqlCommand
+    Public WithEvents btnSave As Button
+    Friend WithEvents Satuan As ColumnHeader
+    Friend WithEvents txtLogs As RichTextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtSatuan As TextBox
+    Friend WithEvents txtHarga As NumericUpDown
+    Friend WithEvents id As ColumnHeader
+
+End Class
---
+++ ActivityLogs/Form1.vb
@@ -0,0 +1,199 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Mysqlx.Crud
+Imports System.IO
+Imports System.IO.File
+Imports System.Text
+
+Public Class Form1
+    Dim sqlConn As New MySqlConnection
+    Dim sqlCmd As New MySqlCommand
+    Dim sqlRd As MySqlDataReader
+    Dim actionStatus As String = "create"
+    Dim selectedId As String = ""
+
+    'Dim sqlDt As New DataTable
+    'Dim sqlDtAdt As New MySqlDataAdapter
+
+    Private Sub WriteLogs(message As String)
+        Dim filepath As String = "D:/Logfile/logs.txt"
+        Dim text As New StringBuilder
+        text.AppendLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "] " + message)
+        File.AppendAllText(filepath, text.ToString())
+
+        Dim getText As String = My.Computer.FileSystem.ReadAllText(filepath)
+        txtLogs.Text = getText
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        WriteLogs("Application stopped.")
+    End Sub
+
+    Private Sub GetAllData()
+        Dim server As String = "localhost"
+        Dim username As String = "root"
+        Dim password As String = ""
+        Dim database As String = "warehouse"
+
+        sqlConn.ConnectionString = "host=" + server + ";user=" + username + ";password=" + password + ";database=" + database
+        sqlCmd.Connection = sqlConn
+
+        Dim sqlDt As New DataTable
+        sqlConn.Open()
+        sqlCmd.CommandText = "SELECT * FROM products"
+        sqlRd = sqlCmd.ExecuteReader
+
+        ListView1.Items.Clear()
+
+        Dim i As Integer = 1
+        While sqlRd.Read()
+            Dim lv = ListView1.Items.Add(i)
+            lv.SubItems.Add(sqlRd.GetUInt32("id"))
+            lv.SubItems.Add(sqlRd.GetString("name"))
+            lv.SubItems.Add(sqlRd.GetDouble("price"))
+            lv.SubItems.Add(sqlRd.GetInt32("stock"))
+            lv.SubItems.Add(sqlRd.GetString("uom"))
+            i = i + 1
+        End While
+
+        sqlRd.Close()
+        sqlConn.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim filepath As String = "D:/Logfile/logs.txt"
+        If Not Exists(filepath) Then
+            Dim fs As FileStream = File.Create(filepath)
+        End If
+
+        WriteLogs("Application started.")
+
+        GetAllData()
+    End Sub
+
+
+    Private Function Validator() As Boolean
+        Dim results As Boolean = If(txtNama.Text IsNot "" And txtHarga.Text > 0 And txtStok.Value > 0 And txtSatuan.Text IsNot "", True, False)
+
+        If Not results Then
+            WriteLogs("Simpan data gagal karena ada kolom yang kosong.")
+        End If
+
+        Return results
+    End Function
+
+    Private Function SaveData() As Boolean
+        'for creating
+        If Not Validator() Then
+            Return False
+        End If
+
+        sqlConn.Open()
+        sqlCmd.CommandText = "INSERT INTO products (name, price, stock, uom) VALUE('" + txtNama.Text + "', '" + txtHarga.Value.ToString() + "', '" + txtStok.Value.ToString() + "', '" + txtSatuan.Text + "' )"
+        sqlCmd.ExecuteNonQuery()
+        sqlConn.Close()
+
+        WriteLogs("Simpan data berhasil.")
+        Return True
+    End Function
+
+    Private Function UpdateData(id As String) As Boolean
+        'for updating
+        If Not Validator() Then
+            Return False
+        End If
+
+        sqlConn.Open()
+        sqlCmd.CommandText = "UPDATE products SET name='" + txtNama.Text + "', price='" + txtHarga.Value.ToString() + "', stock='" + txtStok.Value.ToString() + "', uom='" + txtSatuan.Text + "' WHERE id=" + id
+        sqlCmd.ExecuteNonQuery()
+        sqlConn.Close()
+
+        WriteLogs("Edit data berhasil.")
+        Return True
+    End Function
+
+    Private Sub ResetColumn()
+        txtNama.Text = ""
+        txtHarga.Value = 0
+        txtStok.Value = 0
+        txtSatuan.Text = ""
+    End Sub
+
+    Private Function IsSelected() As Boolean
+        If selectedId IsNot "" Then
+            Return True
+        Else
+            MsgBox("Id belum dipilih")
+            Return False
+        End If
+    End Function
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim status As Boolean
+
+        If actionStatus Is "create" Then
+            status = SaveData()
+        ElseIf actionStatus Is "edit" And IsSelected() Then
+            status = UpdateData(selectedId)
+        End If
+
+        If status Then
+            GetAllData()
+            MsgBox("Operasi Berhasil.")
+            ResetColumn()
+        Else
+            MsgBox("Operasi Gagal!")
+        End If
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        actionStatus = "create"
+
+        'will reset values
+        ResetColumn()
+
+        WriteLogs("Kolom berhasil di reset")
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        actionStatus = "edit"
+
+        Dim index As Integer = ListView1.FocusedItem.Index
+        Dim item = ListView1.Items(index)
+        Dim id As String = item.SubItems(1).Text
+
+        selectedId = id
+
+        'will change values
+        txtNama.Text = item.SubItems(2).Text
+        txtHarga.Value = Integer.Parse(item.SubItems(3).Text)
+        txtStok.Value = Integer.Parse(item.SubItems(4).Text)
+        txtSatuan.Text = item.SubItems(5).Text
+
+        WriteLogs("Nilai kolom berhasil diubah")
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        actionStatus = "delete"
+
+        Dim index As Integer = ListView1.FocusedItem.Index
+        Dim item = ListView1.Items(index)
+        Dim id As String = item.SubItems(1).Text
+
+        selectedId = id
+
+        If IsSelected() Then
+            sqlConn.Open()
+            sqlCmd.CommandText = "DELETE FROM products WHERE id=" + selectedId
+            sqlCmd.ExecuteNonQuery()
+            sqlConn.Close()
+
+            WriteLogs("Data berhasil dihapus")
+            MsgBox("Data berhasil dihapus")
+            ResetColumn()
+            GetAllData()
+        Else
+            WriteLogs("Hapus data gagal")
+            MsgBox("Hapus data gagal")
+        End If
+    End Sub
+End Class
